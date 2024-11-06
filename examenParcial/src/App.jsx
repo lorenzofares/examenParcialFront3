95e36308f7ccf11c86ab5581327af328897da1b9
lorenzofares <lolofaresestudio@gmail.com>
2024-11-06 20:50:05 -0300

    Agregando estructura del examen

---
+++ examenParcial/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <>
+    <div className='App'>
+      <h1>Carga de Estudiantes</h1>
+      <form></form>
+    <Card/>
+    </div>
+    </>
+  );
+}
+
+export default App
